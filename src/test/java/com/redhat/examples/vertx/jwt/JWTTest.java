ac5bcd4ad5224809fbef40d08d1d3fd872e32201
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2017-12-31 14:33:43 -0500

    passing token test

--- src/test/java/com/redhat/examples/vertx/jwt/JWTTest.java
+++ src/test/java/com/redhat/examples/vertx/jwt/JWTTest.java
@@ -5,16 +5,15 @@ import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpClientOptions;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.auth.jwt.JWTAuth;
+import io.vertx.ext.auth.jwt.JWTOptions;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
-import io.vertx.ext.unit.junit.Repeat;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(VertxUnitRunner.class)
@@ -22,7 +21,7 @@ public class JWTTest {
 
   private Vertx vertx;
 
-  private JWTAuth jwtAuth;
+  private String token;
 
   @Before
   public void setUp(TestContext testContext) {
@@ -30,11 +29,12 @@ public class JWTTest {
     vertx.deployVerticle(JWTAuthenticationVerticle.class.getName(), new DeploymentOptions(), testContext.asyncAssertSuccess());
 
     // Configure authentication with JWT
-    jwtAuth = JWTAuth.create(vertx, new JsonObject().put("keyStore", new JsonObject()
+    JWTAuth jwtAuth = JWTAuth.create(vertx, new JsonObject().put("keyStore", new JsonObject()
       .put("type", "jceks")
       .put("path", "keystore.jceks")
       .put("password", "secret")));
 
+    token = jwtAuth.generateToken(new JsonObject().put("sub", "jeremy"), new JWTOptions());
   }
 
   @After
@@ -72,6 +72,23 @@ public class JWTTest {
       .handler(response -> {
         testContext.assertEquals(401, response.statusCode());
         async.complete();
+      }).end();
+  }
+
+  @Test
+  public void testSuccessfulAuthentication(TestContext testContext) {
+    Async async = testContext.async();
+
+    vertx.createHttpClient(new HttpClientOptions()
+      .setSsl(true).setTrustAll(true)).post(8080, "localhost", "/protected/")
+      .putHeader("content-type", "application/json")
+      .putHeader("Authorization", "Bearer " + token)
+      .handler(response -> {
+        testContext.assertEquals(200, response.statusCode());
+        response.bodyHandler(body -> {
+          assertEquals(VerticleMessages.PROTECTED_MESSAGE, body.toString());
+          async.complete();
+        });
       }).end();
   }
 }
